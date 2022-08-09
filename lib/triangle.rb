6e14222786607f5b54c680e9cd9ea505a12bbe77
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-08-09 13:48:19 -0400

    solution

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,93 @@
+# class Triangle
+
+#   # attr_accessor :a, :b, :c
+
+#   # def initalize(a, b, c)
+#   #   @a = a
+#   #   @b = b
+#   #   @c = c
+#   # end
+
+#   # def kind
+#   #   if a == b && b == c
+#   #      :equilateral
+#   #   elsif a == b || b == c || a == c
+#   #     :isosceles
+#   #   else 
+#   #     :scalene
+#   #   end
+#   # end
+
+#   attr_reader :a, :b, :c
+  
+#   def initialize(a, b, c)
+#     @a = a
+#     @b = b
+#     @c = c
+#   end
+
+#   def kind
+#     validate_triangle
+
+#     if a == b && b == c
+#       :equilateral
+#     elsif a == b || b == c || a == c
+#       :isosceles
+#     else
+#       :scalene
+#     end
+#   end
+
+#   def sides_greater_than_zero?
+#     [a, b, c].all?(&:positive)
+#   end
+
+#   def validate_triangle_inequality?
+#     a + b > c && a + c > b && b + c > a
+#   end
+
+#   def validate_triangle
+#     raise TriangleError unless sides_greater_than_zero? && validate_triangle_inequality?
+#   end
+
+#   class TriangleError < StandardError
+#   end
+
+# end
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
